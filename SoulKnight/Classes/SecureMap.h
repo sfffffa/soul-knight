d3bb3dd1a2853ae65f122fcec1946f07245c1ea9
hthcccc <65483560+hthcccc@users.noreply.github.com>
2020-06-14 23:02:54 +0800

    add settingscene and alter securemap

--- SoulKnight/Classes/SecureMap.h
+++ SoulKnight/Classes/SecureMap.h
@@ -1,38 +1,75 @@
 #ifndef _SECURE_MAP_H_
+
 #define _SECURE_MAP_H_
 
+
+
 #include "cocos2d.h"
+
 #include "class/LimitedAttribute.h"
+
 #include "class/AttributeChanger.h"
+
 #include "class/Item.h"
+
 #include "class/ElementalBullet.h"
+
 #include "class/CloseInWeapon.h"
+
 #include "class/RangedWeapon.h"
+
 #include "class/Boss.h"
+
 #include "class/Hero.h"
+
 #include <string>
 
+
+
 #define HERO 10
+
 #define ENEMY 20
+
 #define MY_BULLET 30
+
 #define ENEMY_BULLET 40
+
 #define NPC 50
 
+
+
 class SecureMap : public cocos2d::Scene
+
 {
+
 public:
+
 	static cocos2d::Scene* createScene();
+
+
 
 	virtual bool init();
 
+
+
 	// implement the "static create()" method manually
+
 	CREATE_FUNC(SecureMap);
+
+
 
 private:
 
+	TMXTiledMap* _tiledmap;
+
 	void addNPC(const std::string& spriteFrameName, cocos2d::Vec2 pos);
 
+
+
 	void addHero(std::shared_ptr<Hero> hero);
+
 };
 
-#endif // _SECURE_MAP_H_
+
+
+#endif // _SECURE_MAP_H_#pragma once
