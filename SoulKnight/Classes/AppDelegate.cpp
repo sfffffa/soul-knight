0c9a7869ecd758fb0c8842095e1386e30e50ff2d
hthcccc <65483560+hthcccc@users.noreply.github.com>
2020-06-16 12:59:49 +0800

    更新appdelegate

--- SoulKnight/Classes/AppDelegate.cpp
+++ SoulKnight/Classes/AppDelegate.cpp
@@ -34,10 +34,10 @@ using namespace cocos2d::experimental;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size designResolutionSize = cocos2d::Size(2048, 960);//old 480, 320
 static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);//2048 1536
 
 AppDelegate::AppDelegate()
 {
@@ -73,9 +73,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("SoulKnight", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("Helloworld", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
 #else
-        glview = GLViewImpl::create("SoulKnight");
+        glview = GLViewImpl::create("Helloworld");
 #endif
         director->setOpenGLView(glview);
     }
