84871343d2d911acf850a47b8e4b154b40a2e054
cyf <1042750083@qq.com>
2020-06-16 17:38:25 +0800

    appDelegate修订

--- SoulKnight/Classes/AppDelegate.cpp
+++ SoulKnight/Classes/AppDelegate.cpp
@@ -34,7 +34,6 @@ using namespace cocos2d::experimental;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(2048, 960);//old 480, 320
 static cocos2d::Size designResolutionSize = cocos2d::Size(2048, 1536);
 static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
